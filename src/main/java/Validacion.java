ae4e7b2b25e69f9c7096c7111c2106647b24bb35
cristobal <c.ramos07@ufromail.cl>
2025-04-26 23:05:54 -0400

    actualizacion del metodo validarMatricula, se le añade validacion por patron de matricula y por matricula existente en base de datos (actualmente un arraylist)

--- src/main/java/Validacion.java
+++ src/main/java/Validacion.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
+import java.util.regex.*;
+import java.util.ArrayList;
 
 public class Validacion {
     private static final Scanner sc = new Scanner(System.in);
+    static ArrayList<String> datos = new ArrayList<>(1);
     public static void main(String[] args){
 
     }
     public static String leerMatricula(){
-        System.out.println("ingrese su matrícula");
+        System.out.println("ingrese su matrícula: ");
         return sc.nextLine();
     }
     private static String leerContrasena(){
         System.out.println("ingrese su contraseña:");
         return sc.nextLine();
     }
-    private static Boolean validarMatricula(){
-        return true;
+    public static Boolean validarMatricula(String matricula){
+        String regex = "^[0-9]{8}[0-9Kk][0-9]{2}";
+        boolean dato = datos.contains(matricula);
+
+        return dato && Pattern.matches(regex,matricula);
     }
     private static Boolean validarContrasena(){
         return true;
