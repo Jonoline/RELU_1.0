b8c1b18e18ad3a775e4e3061374bdd0fe916c515
cristobal <c.ramos07@ufromail.cl>
2025-04-28 13:11:01 -0400

    cambios sutiles que me daba el intellij

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Menu {
-    private static Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         reservasLogias.main(new String[0]);
@@ -21,7 +21,7 @@ public class Menu {
         int opcion;
         do {
             mostrarOpciones();
-            opcion = obtenerOpcion(0);
+            opcion = obtenerOpcion();
             if (opcion==4) {
                 ejecutarOpcion(opcion);
                 break;
@@ -48,7 +48,8 @@ public class Menu {
         System.out.print("      Opcion: ");
     }
 
-    public static int obtenerOpcion(int opcion) {
+    public static int obtenerOpcion() {
+        int opcion;
         try {
             opcion = Integer.parseInt(sc.nextLine());
         } catch (NumberFormatException e) {
@@ -89,15 +90,16 @@ public class Menu {
         /*Menu que muestra todas las horas posibles para reservar */
 
         System.out.println("🕒 Seleccione un horario:");
-        System.out.println("[1]   8:30-9:30\n" +
-                "[2]   9:40-10:40\n" +
-                "[3]  10:50-11:50\n" +
-                "[4]  12:00-13:00\n" +
-                "[5]  13:10-14:10\n" +
-                "[6]  14:30-15:30\n" +
-                "[7]  15:40-16:40\n" +
-                "[8]  16:50-17:50\n" +
-                "[9]  18:00-19:00");
+        System.out.println("""
+                [1]   8:30-9:30
+                [2]   9:40-10:40
+                [3]  10:50-11:50
+                [4]  12:00-13:00
+                [5]  13:10-14:10
+                [6]  14:30-15:30
+                [7]  15:40-16:40
+                [8]  16:50-17:50
+                [9]  18:00-19:00""");
     }
     private static void cerrarSesion(){
         boolean accesoPermitido;
