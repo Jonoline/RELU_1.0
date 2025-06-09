da15890228a466d5e0a3f1b05ce416a76f3dcbd5
cristobal <c.ramos07@ufromail.cl>
2025-06-08 21:08:12 -0400

    se añaden metodos a GestorReservas:  agregar, verificarUsuarioNoTengaReserva, cancelar, buscarReservaPorMatricula

--- src/main/java/logica/GestorReservas.java
+++ src/main/java/logica/GestorReservas.java
@@ -1,15 +1,50 @@
 package logica;
 import datos.Json;
+import datos.Logia;
 import datos.Reserva;
+import datos.Usuario;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 public class GestorReservas {
-    private static final String archivo = "reservas.json";
-    private List<Reserva> reservas;
+    private final ArrayList<Reserva> reservas;
+    private final Json json = new Json();
 
     public GestorReservas() {
-        Json json = new Json();
         reservas = new Json().CargarReservas();
     }
+    public void agregarReserva(Usuario usuario, Logia logia, LocalDateTime fechaHora){
+        reservas.add(new Reserva(usuario.getMatricula(), logia, fechaHora));
+        json.IngresarReservas(reservas);
+    }
+    public Reserva buscarReserva(String matricula){
+        for (Reserva r : reservas) {
+            if (r.getMatricula().equals(matricula)) {
+                return r;
+            }
+        }
+        return null;
+    }
+    private Boolean verificarUsuarioNoTengaReserva(String matricula){
+        for (Reserva r : reservas) {
+            if (r.getMatricula().equals(matricula)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void CancelarReserva(Reserva reservaUsuario){
+        reservas.remove(reservaUsuario);
+        json.IngresarReservas(reservas);
+    }
+    public Boolean verificarTimeReserva(){
+        for (Reserva r : reservas) {
+            if (r.getFechaHora().isBefore(LocalDateTime.now())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
